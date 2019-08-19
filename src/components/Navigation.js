9adb760aeef536da092f53f8a79ed48284acd7b4
Min Huang <minh.huang96@gmail.com>
2019-08-19 12:47:50 -0700

    add className

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Route, Link } from "react-router-dom"
-import Home from "./Home"
-import About from "./About"
-import Contact from "./Contact"
+
 
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
-        <Link to="/">Home</Link>
-        <Link to="/About">About</Link>
-        <Link to="/Contact">Contact</Link>
+        <Link className="navButton Home" to="/">
+          Home
+        </Link>
+        <Link className="navButton About" to="/About">
+          About
+        </Link>
+        <Link className="navButton Contact" to="/Contact">
+          Contact
+        </Link>
       </div>
-      
     </div>
   );
 };
